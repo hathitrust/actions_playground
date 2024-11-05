ad13379de5f803b54a663d0aa537fccf8c5b4aad
K'Ron Spar <kspar@umich.edu>
2024-11-05 14:49:56 -0500

    Updated python code since there's no running server to work with.

---
+++ server.py
@@ -0,0 +1,8 @@
+from http.server import SimpleHTTPRequestHandler
+import socketserver
+
+PORT = 7000
+
+def run_server():
+    with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd: print(f"Serving on port {PORT}")
+    httpd.serve_forever()
--- test_python_server.py
+++ test_python_server.py
@@ -1,7 +1,16 @@
 import unittest
 import requests
+import threading
+import time
+from server import run_server
 
 class TestPythonServer(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.server_thread = threading.Thread(target=run_server)
+        cls.server_thread.daemon = True
+        cls.server_thread.start()
+        time.sleep(1)
     def test_index_htlp_served(self):
         response = requests.get("http://localhost:7000/index.html")
         self.assertEqual(response.status_code, 200)
